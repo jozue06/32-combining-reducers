27c48fd558dc035febbdd7f53bef2939472d6999
Jozue06 <jozue06@gmail.com>
2018-08-18 15:46:17 -0700

    eh test

--- unused/index.js
+++
@@ -1,4 +0,0 @@
-import {createStore} from 'redux'
-import reducer from '../lib/categories.js'
-
-export default () => createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
